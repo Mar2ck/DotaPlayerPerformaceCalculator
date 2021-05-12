1d56a399be2c1a53f40ae5a3783f47848e02ad43
mbreitwieser <19704575@students.lincoln.ac.uk>
2021-05-12 13:26:52 +0100

    Small fixes and changes to ui Removed unnecessary function

--- src/main.py
+++ src/main.py
@@ -18,25 +18,29 @@ def DisplayMenuOptions():
 
 def SetUpMatch():
     menuChoice = ""
+    print ("Sorting all players...")
     sortedPlayers = MergeSort(data_scraper.ListAllPlayerIDs())
+    print ("Sorted " + str(len(data_scraper.ListAllPlayerIDs())) + "players total")
     team1 = []
     team2 = []
     while (menuChoice != "9"):
 
-        if (len(team1) > 0 or len(team2) > 0):
+        if (len(team1) > 0):
             print ("Team 1: ")
             for i in team1:
-                ##print (data_scraper.DictPlayerInfo(i)["player_id"])
-                ##print (i)
                 print (data_scraper.DictPlayerInfo(i)["name"])
+        else:
+            print ("Team 1 is empty")
+        if (len(team2) > 0):
             print ("Team 2: ")
             for i in team2:
                 print (data_scraper.DictPlayerInfo(i)["name"])
+        else:
+            print("Team 2 is empty")
 
-            if (len(team1) > 0 and len(team2) > 0):
-
-                prob = player_performance_score.GetProbability(AveragePPS(team1),AveragePPS(team2))
-                print ("There is a " + str(prob * 100) + "% chance that team 1 will beat team 2")
+        if (len(team1) > 0 and len(team2) > 0):
+            prob = player_performance_score.GetProbability(AveragePPS(team1),AveragePPS(team2))
+            print ("There is a " + str(prob * 100) + "% chance that team 1 will beat team 2")
 
         print ("1: Add player to team 1")
         print ("2: Add player to team 2")
@@ -50,14 +54,14 @@ def SetUpMatch():
                 if (menuChoice == 1):
                     temp = GetPlayer(sortedPlayers)
                     if (temp != -1):
-                        team1.append(GetPlayer(sortedPlayers))
+                        team1.append(temp)
                     else:
                         print ("Player not found")
                     break
                 elif (menuChoice == 2):
                     temp = GetPlayer(sortedPlayers)
                     if (temp != -1):
-                        team2.append(GetPlayer(sortedPlayers))
+                        team2.append(temp)
                     else:
                         print("Player not found")
                     break
@@ -76,9 +80,6 @@ def GetPlayer(allPlayers): ##Get player id from user's player selection
     name = input("Please enter the player's name: ")
     playerID = SearchPlayers(allPlayers, name)
     return playerID
-
-def ListPlayers():
-    pass
 
 def SearchPlayers(array, item): ##Searches for a player based on name
     mIndex = (len(array)) // 2
@@ -113,7 +114,7 @@ def MergeSort(array): ##Sorts the array based on name
         k = 0
 
         while i < len(left) and j < len(right):
-            if (data_scraper.DictPlayerInfo(left[i])["name"] < data_scraper.DictPlayerInfo(right[j])["name"]):  ##Look at how I compare two items when I know what the items will be
+            if (data_scraper.DictPlayerInfo(left[i])["name"] < data_scraper.DictPlayerInfo(right[j])["name"]):
                 array[k] = left[i]
                 i += 1
             else:
@@ -146,9 +147,9 @@ while True:
         if (menuChoice == 1):
             SetUpMatch()
         elif (menuChoice == 2):
-            ListPlayers()
+            pass
         elif (menuChoice == 9):
             break
         else:
-            print("Be better")  ##Checks if the int is an option
+            print("Be better")
 print("Thanks for coming")

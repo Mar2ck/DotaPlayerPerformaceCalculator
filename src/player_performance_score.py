17ce12b26bb99efe7ca68b6e4bbbb098a0ac6179
mbreitwieser <19704575@students.lincoln.ac.uk>
2021-05-13 13:13:47 +0100

    Added comments to player_performance_score.py

--- src/player_performance_score.py
+++ src/player_performance_score.py
@@ -7,38 +7,38 @@ import json
 # Local modules
 import data_scraper
 
-playerPPS = {}
+playerPPS = {} ##Creates an empty dictionary to store player's pps
 
 
 def PlayerContributionScore(match_id, player_id):
     score = 0.0
-    killWorth = 200
-    playerMatchStats = data_scraper.DictPlayerMatchStats(match_id, player_id)
+    killWorth = 200 ##Kill weight can be altered
+    playerMatchStats = data_scraper.DictPlayerMatchStats(match_id, player_id) ##Gets the player's performance in the match
     score += playerMatchStats["kills"] * killWorth
     score += playerMatchStats["assists"] * killWorth * 0.5
     score += playerMatchStats["deaths"] * killWorth * -1
     score += playerMatchStats["xp_per_min"] * 1
     score += playerMatchStats["gold_per_min"] * 1
-    return score
+    return score ##Returns the total contribution score
 
 
 def PlayerContributionPercentage(match_id, player_id):
     playersArray = data_scraper.ListPlayersinMatch(match_id)
     teamContributionTotal = 0.0
-    for player_id in playersArray:
+    for player_id in playersArray: ##Loops through every player's contibutions
         teamContributionTotal += PlayerContributionScore(match_id, player_id)
     return PlayerContributionScore(match_id, player_id)/teamContributionTotal
 
 
 def GetProbability(ppsA, ppsB):
-    return 1/(1+10**((ppsB - ppsA)/400))  # if the difference is 400 the chance of winning is 10x high
+    return 1/(1+10**((ppsB - ppsA)/400))  ##If the difference is 400 the chance of winning is 10x high
 
 
 def CalculatePPSChange(ppsA, ppsB):
-    eloConstant = 32  # Can be changed
-    pA = GetProbability(ppsA, ppsB)
-    temp = round(eloConstant * (1 - pA), 0)
-    change = int(temp)  # Change is always an int
+    eloConstant = 32  ##Elo constant can be altered
+    pA = GetProbability(ppsA, ppsB) ##Gets the probability of player A winning
+    temp = round(eloConstant * (1 - pA), 0) ##Converts the change into an integer
+    change = int(temp)  ##Change is always an int
     return change
 
 
@@ -49,36 +49,36 @@ def GetPPS(playerID):
 
 
 def InitialPPS():
-    for i in data_scraper.ListAllPlayerIDs():
-        playerPPS[i] = 1000
+    for i in data_scraper.ListAllPlayerIDs(): ##Loops through every player
+        playerPPS[i] = 1000 ##Sets the player's PPS to 1000
 
 
 def AveragePPS(team):
     total = 0
-    for i in team:
-        total += playerPPS.get(i)
-    return total//i
+    for i in team: ##Loops through each player in the team
+        total += playerPPS.get(i) ##Adds the player's PPS to the total
+    return total//i ##Returns the mean average PPS
 
 
 def SetPlayersNewPPS():
     print ("Updating player PPS...")
-    for match in data_scraper.matchArray:
+    for match in data_scraper.matchArray: ##Loops through all matches
         team1 = []
         team2 = []
         i = 0
-        for player in match["players"]:
-            if player["isRadiant"]:
-                team1.append(player["account_id"])
+        for player in match["players"]: ##Loops through each player in the match
+            if player["isRadiant"]: ##Checks if the player is on team 1 or 2
+                team1.append(player["account_id"]) ##Adds the player to the team
             else:
                 team2.append(player["account_id"])
-        changeInPPS = 5 * (CalculatePPSChange(AveragePPS(team1), AveragePPS(team2)))
-        if match["radiant_win"]:
-            for player in match["players"]:
-                weight = PlayerContributionPercentage(match["match_id"], player["account_id"])
-                if player["isRadiant"]:
-                    playerPPS[player["account_id"]] += changeInPPS * weight
+        changeInPPS = 5 * (CalculatePPSChange(AveragePPS(team1), AveragePPS(team2))) ##Calculates the change in PPS based on the averages of the two teams
+        if match["radiant_win"]: ##Checks if team 1 won
+            for player in match["players"]: ##Loops through each player in the match
+                weight = PlayerContributionPercentage(match["match_id"], player["account_id"]) ##Works out the individual player's PPS change based on their contribution
+                if player["isRadiant"]: ##Checks if the player was on the winning team
+                    playerPPS[player["account_id"]] += changeInPPS * weight ##Increases the player's PPS if they won
                 else:
-                    playerPPS[player["account_id"]] -= changeInPPS * weight
+                    playerPPS[player["account_id"]] -= changeInPPS * weight ##Decreases the player's PPS if they lost
         else:
             for player in match["players"]:
                 weight = PlayerContributionPercentage(match["match_id"], player["account_id"])
@@ -89,10 +89,8 @@ def SetPlayersNewPPS():
 
 
 #### Main ####
-InitialPPS()
-SetPlayersNewPPS()
-# for i in data_scraper.ListAllPlayerIDs():
-#     print(playerPPS[i])
+InitialPPS() ##Sets every player's PPS to 1000
+SetPlayersNewPPS() ##Calculates new PPS based on all match data
 if __name__ == "__main__":
     print(data_scraper.DictPlayerInfo(135878232))
     print(data_scraper.DictPlayerMatchStats(4967600837, 135878232))
